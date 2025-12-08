89f2e6941149d5ca8804c7df8927cf8f9d7f5703
ThinkForFunSol <slimestuff@tuta.com>
2025-12-08 19:34:36 +0800

    Create SearchBar.tsx

---
+++ app/src/components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    onSearch(e.target.value);
+  };
+
+  return (
+    <input type="text" placeholder="Search ideas..." value={query} onChange={handleChange} style={{ width: '100%', padding: '10px' }} />
+  );
+};
+
+export default SearchBar;
